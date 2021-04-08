6f61ca587c9c023825033a9d871700bfd7403040
zs-richy <zs.ricsi@gmail.com>
2021-04-08 14:11:58 +0200

    Main Menu and Game View scenes added

--- src/main/java/controller/mainMenuController.java
+++ src/main/java/controller/mainMenuController.java
@@ -1,4 +1,27 @@
 package controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class mainMenuController {
+
+    public void loadScene(Stage _stage, String _fxml) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/" + _fxml + ".fxml"));
+        Parent root = fxmlLoader.load();
+        Stage stage = _stage;
+        stage.setScene(new Scene(root));
+        stage.show();
+    }
+
+    public void startPressed(ActionEvent e) throws IOException {
+        Stage scene = (Stage) ((Node) e.getSource()).getScene().getWindow();
+        loadScene(scene, "gameView");
+    }
+
 }
