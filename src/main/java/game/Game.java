db08bfd54ff6895578cc4ebf87862fc9a8039792
zs-richy <zs.ricsi@gmail.com>
2021-04-08 15:00:44 +0200

    Set the core field and solution for the game

---
+++ src/main/java/game/Game.java
@@ -0,0 +1,44 @@
+package game;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Game {
+
+    private List<String> solution;
+    private List<List<String>> field;
+
+    public Game() {
+        solution = initSolution();
+        this.field = initField();
+    }
+
+    private List<String> initSolution() {
+        List<String> solution;
+        solution = Arrays.asList("I","II","III","IV","V","VI","VII","VIII","IX","X",
+                "XI","XII","XIII","XIV","XV","XVI","XVII","XVIII","XIX","XX",
+                "XXI","XXII","XXIII","XXIV","XXV","XXVI","XXVII", "XXVIII","XXIX","XXX",
+                "XXXI","XXXII","XXXIII","XXXIV","XXXV","XXXVI","XXXVII","XXXVIII","XXXIX","XL");
+
+        return solution;
+    }
+
+    private List<List<String>> initField() {
+        List<List<String>> field;
+        field = Arrays.asList(
+                Arrays.asList("I", "I", "X", "V", "", "L", "X"),
+                Arrays.asList("X", "L", "", "I", "X", "V", "I"),
+                Arrays.asList("", "I", "V", "I", "X", "", "X"),
+                Arrays.asList("L", "I", "X", "", "V", "I", "X"),
+                Arrays.asList("X", "", "X", "I", "X", "I", ""),
+                Arrays.asList("L", "I", "V", "L", "", "X", "X"),
+                Arrays.asList("V", "I", "", "X", "I", "X", "L")
+        );
+
+        return field;
+    }
+
+
+}
