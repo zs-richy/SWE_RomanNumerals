3b1a6ff7c12e462f9357d2f81dd4c191869133e9
zs-richy <zs.ricsi@gmail.com>
2021-04-08 13:46:19 +0200

    Added core sources for OpenJFX application

---
+++ src/main/java/main/RomanNumeralsApplication.java
@@ -0,0 +1,20 @@
+package main;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class RomanNumeralsApplication extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/mainMenu.fxml"));
+        stage.setTitle("Logic Game");
+        stage.setResizable(false);
+        stage.setScene(new Scene(root));
+        stage.show();
+    }
+
+}
