3ba32d71e2f72068b4813f085691777e058d220f
zs-richy <zs.ricsi@gmail.com>
2021-04-09 11:32:30 +0200

    Cleaning up the code in gameViewController

--- src/main/java/controller/gameViewController.java
+++ src/main/java/controller/gameViewController.java
@@ -2,27 +2,17 @@ package controller;
 
 import game.Direction;
 import game.Game;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.geometry.Pos;
 import javafx.scene.Node;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
-import javafx.stage.Stage;
 import org.pmw.tinylog.Logger;
-
-import java.io.IOException;
-import java.security.Key;
 import java.util.ArrayList;
-import java.util.EventObject;
 
 public class gameViewController {
     Game game;
@@ -50,19 +40,23 @@ public class gameViewController {
         return color;
     }
 
-    public void move() {
+    public void preMove() {
         Label currentLabel = labels.get(game.getCurrentX()).get(game.getCurrentY());
         currentLabel.setBorder(new Border(new BorderStroke(Color.BLACK,
                 BorderStrokeStyle.SOLID, CornerRadii.EMPTY, BorderWidths.DEFAULT)));
     }
 
+    public void postMove() {
+        labels.get(game.getCurrentX()).get(game.getCurrentY()).setBorder(new Border(new BorderStroke(Color.DARKBLUE,
+                BorderStrokeStyle.DASHED, CornerRadii.EMPTY, new BorderWidths(4, 4, 4, 4))));
+    }
+
     @FXML
     public void gridPaneKeyPressed(KeyEvent e) {
         Logger.info("Key pressed on gridPane.");
-        move();
+        preMove();
         if (e.getCode() == KeyCode.UP) {
             Logger.info("UP key");
-            move();
             game.move(Direction.UP);
         }
         if (e.getCode() == KeyCode.DOWN) {
@@ -77,8 +71,7 @@ public class gameViewController {
             Logger.info("RIGHT key");
             game.move(Direction.RIGHT);
         }
-        labels.get(game.getCurrentX()).get(game.getCurrentY()).setBorder(new Border(new BorderStroke(Color.DARKBLUE,
-                BorderStrokeStyle.DASHED, CornerRadii.EMPTY, new BorderWidths(4, 4, 4, 4))));
+        postMove();
     }
 
     @FXML
