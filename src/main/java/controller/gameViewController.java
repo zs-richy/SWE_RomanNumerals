46dabf0e16263204ae968fbe84cc1a8c7208d08c
zs-richy <zs.ricsi@gmail.com>
2021-04-10 18:02:12 +0200

    gameViewController now checks for lost/won game state and updates gameView accordingly

--- src/main/java/controller/gameViewController.java
+++ src/main/java/controller/gameViewController.java
@@ -2,16 +2,25 @@ package controller;
 
 import game.Direction;
 import game.Game;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.geometry.Pos;
 import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
+import javafx.stage.Stage;
 import org.pmw.tinylog.Logger;
+
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class gameViewController {
@@ -24,7 +33,20 @@ public class gameViewController {
     GridPane gridPane;
     @FXML
     AnchorPane mainPane;
-
+    @FXML
+    AnchorPane paneGame;
+    @FXML
+    AnchorPane paneResult;
+    @FXML
+    AnchorPane paneStart;
+    @FXML
+    Button startButton;
+    @FXML
+    Button mainMenuButton;
+    @FXML
+    TextField nameField;
+    @FXML
+    Label resultLabel;
 
     public String getColor(int x, int y) {
         String fieldXY = game.getFieldXY(x,y);
@@ -40,6 +62,7 @@ public class gameViewController {
         return color;
     }
 
+
     public void preMove() {
         Label currentLabel = labels.get(game.getCurrentX()).get(game.getCurrentY());
         currentLabel.setBorder(new Border(new BorderStroke(Color.BLACK,
@@ -49,6 +72,11 @@ public class gameViewController {
     public void postMove() {
         labels.get(game.getCurrentX()).get(game.getCurrentY()).setBorder(new Border(new BorderStroke(Color.DARKBLUE,
                 BorderStrokeStyle.DASHED, CornerRadii.EMPTY, new BorderWidths(4, 4, 4, 4))));
+        if (game.getWon()) {
+            updateViewWon();
+        } else if(game.getLost()) {
+            updateViewLost();
+        }
     }
 
     @FXML
@@ -74,6 +102,41 @@ public class gameViewController {
         postMove();
     }
 
+    public void updateViewWon() {
+        paneGame.setDisable(true);
+        resultLabel.setText("Congrats  " + game.getPlayerName() + "!\nYou completed the puzzle!");
+        resultLabel.setTextFill(Color.GREEN);
+        resultLabel.setAlignment(Pos.CENTER);
+        paneGame.setOpacity(0.1);
+        paneResult.setVisible(true);
+    }
+
+    public void updateViewLost() {
+        paneGame.setDisable(true);
+        resultLabel.setText("You failed " + game.getPlayerName());
+        resultLabel.setTextFill(Color.RED);
+        resultLabel.setAlignment(Pos.CENTER);
+        paneGame.setOpacity(0.1);
+        paneResult.setVisible(true);
+    }
+
+    public void loadMainMenu(ActionEvent e) throws IOException {
+        Stage stage = (Stage) ((Node) e.getSource()).getScene().getWindow();
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/mainMenu.fxml"));
+        stage.setScene(new Scene(root));
+        stage.show();
+    }
+
+    public void startButtonClicked() {
+        if (nameField.getText().equals("")) {
+            game.setPlayerName("Anonymous");
+        } else {
+            game.setPlayerName(nameField.getText());
+        }
+        paneGame.setVisible(true);
+        paneStart.setVisible(false);
+    }
+
     @FXML
     public void initialize() {
         gridPane.setFocusTraversable(true);
@@ -87,11 +150,11 @@ public class gameViewController {
             ArrayList<Label> iterateLabel = new ArrayList<Label>();
             for (int j = 0; j < 7; j++) {
                 Label gridLabel = new Label();
-                gridLabel.setPrefWidth(30);
-                gridLabel.setPrefHeight(30);
+                gridLabel.setPrefWidth(40);
+                gridLabel.setPrefHeight(40);
                 gridLabel.setAlignment(Pos.CENTER);
                 gridLabel.setText(game.getFieldXY(i,j));
-                gridLabel.setFont((new Font(15)));
+                gridLabel.setFont((new Font(20)));
                 gridLabel.setStyle("-fx-font-weight: bold; -fx-background-color: " + getColor(i,j) + ";");
                 gridLabel.setBorder(new Border(new BorderStroke(Color.BLACK,
                         BorderStrokeStyle.SOLID, CornerRadii.EMPTY, BorderWidths.DEFAULT)));
@@ -105,5 +168,6 @@ public class gameViewController {
                 BorderStrokeStyle.DASHED, CornerRadii.EMPTY, new BorderWidths(4, 4, 4, 4))));
 
     }
+
 
 }
