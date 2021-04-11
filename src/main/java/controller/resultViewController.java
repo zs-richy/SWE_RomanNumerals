776cbd7416020e5889209acdababdf60828f3486
zs-richy <zs.ricsi@gmail.com>
2021-04-11 12:21:35 +0200

    resultViewController init method implemented, can load results from resultContainer into table in resultView

--- src/main/java/controller/resultViewController.java
+++ src/main/java/controller/resultViewController.java
@@ -1,4 +1,45 @@
 package controller;
 
+import game.Result;
+import game.resultContainer;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 public class resultViewController {
+
+    @FXML
+    TableView resultTable;
+    @FXML
+    TableColumn nameColumn;
+    @FXML
+    TableColumn stateColumn;
+    @FXML
+    TableColumn timeColumn;
+    @FXML
+    TableColumn dateColumn;
+
+    @FXML
+    public void initialize() {
+        ArrayList<Result> results = resultContainer.getResults();
+        //nameColumn
+        nameColumn.setCellValueFactory(new PropertyValueFactory<>("name"));
+
+        //allegianceColumn
+        stateColumn.setCellValueFactory(new PropertyValueFactory<>("state"));
+
+        //positionColumn
+        timeColumn.setCellValueFactory(new PropertyValueFactory<>("time"));
+
+        //salaryColumn
+        dateColumn.setCellValueFactory(new PropertyValueFactory<>("date"));
+
+        resultTable.getItems().addAll(results);
+
+    }
+
 }
