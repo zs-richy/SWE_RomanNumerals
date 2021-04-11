f8406b2723ad098064ef89ef347f2624172831a4
zs-richy <zs.ricsi@gmail.com>
2021-04-11 11:52:43 +0200

    Added resultContainer which will hold the results

---
+++ src/main/java/game/resultContainer.java
@@ -0,0 +1,18 @@
+package game;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
+public class resultContainer {
+    private static ArrayList<Result>  results = new ArrayList<>();
+
+    public static void addResult(Result result) {
+        results.add(result);
+    }
+
+    public static void orderResults() {
+        Collections.sort(results);
+    }
+
+}
