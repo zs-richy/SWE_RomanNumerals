990e57e4dc112097ecbb4ab9f016c39e2abbd8fa
zs-richy <zs.ricsi@gmail.com>
2021-04-12 23:24:48 +0200

    Changed the results file location to save and read from

--- src/main/java/game/resultContainer.java
+++ src/main/java/game/resultContainer.java
@@ -9,6 +9,7 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
@@ -36,7 +37,7 @@ public class resultContainer {
 
         try {
             ClassLoader loader = Thread.currentThread().getContextClassLoader();
-            FileWriter writer = new FileWriter("results.json");
+            FileWriter writer = new FileWriter(Paths.get(".").toAbsolutePath().normalize().toString()+"/results/results.json");
             //FileWriter writer = new FileWriter(loader.getResource("results/results.json").getFile());
             om.writeValue(writer, results);
             writer.close();
@@ -51,7 +52,7 @@ public class resultContainer {
 
         try {
             ClassLoader loader = Thread.currentThread().getContextClassLoader();
-            Result[] res = om.readValue(new File("results.json"), Result[].class);
+            Result[] res = om.readValue(new File(Paths.get(".").toAbsolutePath().normalize().toString()+"/results/results.json"), Result[].class);
             //Result[] res = om.readValue(new FileReader(loader.getResource("results/results.json").getFile()), Result[].class);
             results = new ArrayList<>(Arrays.asList(res));
             Logger.info("Game results read from file.");
