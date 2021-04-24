1be9e9bfbe91ce2f3e1c5c74460a6e4a7121d20d
zs-richy <zs.ricsi@gmail.com>
2021-04-24 10:50:00 +0200

    Added test classes for Game and Result classes

---
+++ src/test/java/game/GameTest.java
@@ -0,0 +1,81 @@
+package game;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GameTest {
+
+    Game testGame;
+
+    @Test
+    void getFieldXY() {
+        testGame = new Game();
+        assertEquals("I", testGame.getFieldXY(0,0));
+    }
+
+    @Test
+    void canMove() {
+        testGame = new Game();
+        assertTrue(testGame.canMove(Direction.UP));
+        assertTrue(testGame.canMove(Direction.RIGHT));
+        assertTrue(testGame.canMove(Direction.DOWN));
+        assertTrue(testGame.canMove(Direction.LEFT));
+        testGame.setCurrentX(6);
+        testGame.setCurrentY(6);
+        assertFalse(testGame.canMove(Direction.DOWN));
+    }
+
+    @Test
+    void move() {
+        testGame = new Game();
+        testGame.move(Direction.UP);
+        assertEquals(2,testGame.getCurrentY());
+        testGame.move(Direction.DOWN);
+        assertEquals(3, testGame.getCurrentY());
+        testGame.move(Direction.LEFT);
+        assertEquals(2, testGame.getCurrentX());;
+        testGame.move(Direction.RIGHT);
+        assertEquals(3, testGame.getCurrentX());
+    }
+
+    @Test
+    void updateState() {
+        testGame = new Game();
+        testGame.move(Direction.UP);
+        testGame.move(Direction.DOWN);
+        assertEquals("", testGame.getState());
+    }
+
+    @Test
+    void isCorrectState() {
+        testGame = new Game();
+        testGame.setState("I");
+        assertTrue(testGame.isCorrectState());
+    }
+
+    @Test
+    void checkWinCondition() {
+        testGame = new Game();
+        testGame.setStateCounter(40);
+        testGame.checkWinCondition();
+        assertTrue(testGame.isWon());
+    }
+
+    @Test
+    void setLoseCondition() {
+        testGame = new Game();
+        testGame.setLoseCondition();
+        assertTrue(testGame.isLost());
+    }
+
+    @Test
+    void calculateResult() {
+        testGame = new Game();
+        testGame.setStartTimer();
+        testGame.setEndTime(testGame.getStartTime());
+        testGame.calculateResult();
+        assertEquals(0, testGame.getResult().getTime());
+        assertEquals(0, testGame.getResult().getState());
+    }
+}
---
+++ src/test/java/game/ResultTest.java
@@ -0,0 +1,27 @@
+package game;
+
+import org.junit.jupiter.api.Test;
+
+import java.time.format.DateTimeFormatter;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ResultTest {
+
+    @Test
+    void testToString() {
+        Result testResult = new Result("Test", 0, 0);
+        assertEquals("Result{name='Test', state=0, time=0.0, date='"
+                + java.time.LocalDate.now().format(DateTimeFormatter.ofPattern("yyyy.MM.dd.")) + "'}",
+                testResult.toString());
+    }
+
+    @Test
+    void compareTo() {
+        Result testResult1 = new Result("Test", 0, 0);
+        Result testResult2 = new Result("Test", 1, 0);
+        Result testResult3 = new Result("Test", 1, 1.0);
+        assertEquals(-1,testResult1.compareTo(testResult2));
+        assertEquals(1,testResult2.compareTo(testResult3));
+    }
+}
